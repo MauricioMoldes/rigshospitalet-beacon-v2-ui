e917802465e7600e4f75eb67e337ac40e555a0c7
mauricio <mauriciomoldesquaresma@gmail.com>
2024-12-10 10:48:49 +0100

    rigshospitalet logo, debug settings and compose

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3pdl)^a8o6#bft2!5$abk4+8eq=(xbc$xow$-c3+8xw=cr%u%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
